6420a10dc2f091b6de3db2d17b430356d8925137
Dave Blaschke <deblaschke@yahoo.com>
2025-02-08 11:20:21 -0600

    JavaScript enhancements

--- js/slideshow.js
+++ js/slideshow.js
@@ -19,6 +19,8 @@ var slideshowElems = document.getElementsByClassName("concertPix");
 var isMobileDevice = false;
 // Map slide names to audio clips (initialized in slideshow.html)
 // var mapSlideToAudio = new Map([]);
+// Slide count for current audio clip (pause audio if count goes negative due to user interaction)
+var songSlideCount = 0;
 
 // Allow for override of default behavior in URL via query parameters
 if ("URLSearchParams" in window) {
@@ -47,8 +49,9 @@ if ("URLSearchParams" in window) {
   }
 }
 
-// playAudio creates and plays audio object where path is audio clip path
-function playAudio(path) {
+// playAudio creates audio object where path is audio clip path and start indicates whether
+// to play clip
+function playAudio(path, start) {
   // Pause current audio object if one exists
   if (slideshowSound != null) {
     slideshowSound.pause();
@@ -67,10 +70,12 @@ function playAudio(path) {
     }, false);
   }
 
-  // Play audio object, catching/ignoring any errors
-  promise = slideshowSound.play();
-  if (promise) {
-    promise.catch(function(error) { });
+  // Play audio object if indicated, catching/ignoring any errors
+  if (start) {
+    promise = slideshowSound.play();
+    if (promise) {
+      promise.catch(function(error) { });
+    }
   }
 }
 
@@ -91,12 +96,14 @@ function reduceSlideshow() {
   }
   SLIDESHOW_DATE = null;
 }
+
 // hidePlayButton hides play/pause button for manual slideshows
 function hidePlayButton() {
   document.getElementById("buttonPlayPause").style.display = "none";
 }
 
-// toggleFlow plays/pauses slideshow where elem is play/pause button
+// toggleFlow plays/pauses slideshow as result of user action (mouseclick or keystroke)
+// where elem is play/pause button
 function toggleFlow(elem) {
   if (slideshowTimeout != null) {
     // Pause slideshow
@@ -126,20 +133,39 @@ function toggleFlow(elem) {
   }
 }
 
-// changePic changes slide index where n is delta (+1 or -1)
+// changePic changes slide as result of user action (mouseclick or keystroke)
+// where n is delta (+1 or -1)
 function changePic(n) {
-  showPic(slideIndex += n);
+  showPic(n);
 
   if (!MANUAL_SLIDESHOW) {
     // Automatic slideshow
 
     if (slideshowTimeout != null) {
+      // Slideshow playing
+
+      // Set new timeout for new slide
       clearInterval(slideshowTimeout);
       slideshowTimeout = setInterval(slideshow, SLIDESHOW_INTERVAL);
 
-      // Play audio if it exists
       if (slideshowSound != null) {
-        slideshowSound.play();
+        // Load silent audio clip and clear song title if user backed up past beginning of song,
+        // otherwise play audio
+        if (songSlideCount < 0 ) {
+          slideshowSound.pause();
+          playAudio("media/silence.mp3", false);
+          document.getElementById("slideSong").innerHTML = "&nbsp;";
+        } else {
+          slideshowSound.play();
+        }
+      }
+    } else {
+      // Slideshow paused
+
+      // Load silent audio clip and clear song title if user backed up past beginning of song
+      if (songSlideCount < 0 ) {
+        playAudio("media/silence.mp3", false);
+        document.getElementById("slideSong").innerHTML = "&nbsp;";
       }
     }
   } else {
@@ -152,40 +178,20 @@ function changePic(n) {
   }
 }
 
-// showPic displays slide where n is slide index
+// showPic displays slide where n is change to slideIndex
 function showPic(n) {
+  songSlideCount += n;
+  slideIndex += n;
+
   // Reduce slideshow to specific date (one time only)
   if (SLIDESHOW_DATE) reduceSlideshow();
-
-  // Handle wrapping past end of slideshow
-  if (n > slideshowElems.length) {slideIndex = 1}
-
-  // Handle wrapping before beginning of slideshow
-  if (n < 1) {slideIndex = slideshowElems.length}
-
-  // Set all slides to hidden
-  var i;
-  for (i = 0; i < slideshowElems.length; i++) {
-    slideshowElems[i].style.display = "none";
-  }
-
-  // Set current slide to visible
-  slideshowElems[slideIndex-1].style.display = "block";
-
-  // Set slide description
-  document.getElementById("slideName").innerHTML = getDescription(slideshowElems[slideIndex-1].src);
-}
-
-// slideshow begins automatic slideshow
-function slideshow() {
-  // Reduce slideshow to specific date (one time only)
-  if (SLIDESHOW_DATE) reduceSlideshow();
-
-  slideIndex++;
 
   // Handle wrapping past end of slideshow
   if (slideIndex > slideshowElems.length) {slideIndex = 1}
 
+  // Handle wrapping before beginning of slideshow
+  if (slideIndex < 1) {slideIndex = slideshowElems.length}
+
   // Set all slides to hidden
   var i;
   for (i = 0; i < slideshowElems.length; i++) {
@@ -196,24 +202,40 @@ function slideshow() {
   slideshowElems[slideIndex-1].style.display = "block";
 
   // Set slide description
-  var slideName = getDescription(slideshowElems[slideIndex-1].src);
-  document.getElementById("slideName").innerHTML = slideName;
+  var description = getDescription(slideshowElems[slideIndex-1].src);
+  document.getElementById("slideName").innerHTML = description;
+
+  // Clear song title if slide description empty
+  if (description.includes("&nbsp;")) {
+    document.getElementById("slideSong").innerHTML = "&nbsp;";
+  }
 
   // Change audio based on slide name
-  if (SLIDESHOW_AUDIO && typeof(mapSlideToAudio) === 'object') {
+  if (SLIDESHOW_AUDIO && !MANUAL_SLIDESHOW && typeof(mapSlideToAudio) === 'object') {
     // Slide description contains " - " whereas title/credits do not
-    var index = slideName.trim().indexOf(' ');
+    var index = description.trim().indexOf(' ');
     if (index != -1) {
       // Change audio if slide in mapSlideToAudio
-      slideName = slideName.substring(0, index);
+      var slideName = description.substring(0, index);
       if (mapSlideToAudio.has(slideName)) {
-        playAudio(mapSlideToAudio.get(slideName));
+        var mapObj = mapSlideToAudio.get(slideName);
+        playAudio(mapObj.file, slideshowTimeout != null);
+        document.getElementById("slideSong").innerHTML = "&#9835; \"" + mapObj.title + "\" &#9835;";
+        songSlideCount = 0;
       }
     } else if (slideshowSound != null) {
       // Pause audio during title/credits
       slideshowSound.pause();
+
+      // Clear song title during title/credits
+      document.getElementById("slideSong").innerHTML = "&nbsp;";
     }
   }
+}
+
+// slideshow begins automatic slideshow
+function slideshow() {
+  showPic(1);
 
   // Play slideshow if paused
   if (slideshowTimeout == null) {
@@ -221,7 +243,7 @@ function slideshow() {
   }
 }
 
-// Handle left and right arrow keys
+// Handle left arrow, right arrow and pause keys
 document.onkeydown = function(event) {
   switch (event.key) {
     case 'ArrowLeft':
@@ -229,6 +251,11 @@ document.onkeydown = function(event) {
       break;
     case 'ArrowRight':
       changePic(1);
+      break;
+    case 'Escape':
+      if (!MANUAL_SLIDESHOW) {
+        toggleFlow(document.getElementById("buttonPlayPause"));
+      }
       break;
   }
 }
@@ -258,9 +285,9 @@ function setPicDimensions() {
 // Handle window load
 window.onload = function() {
   if (SLIDESHOW_AUDIO) {
-    if (typeof(mapSlideToAudio) !== 'object') {
+    if (typeof(mapSlideToAudio) !== 'object' || MANUAL_SLIDESHOW) {
       // Play default audio
-      playAudio("media/audio.mp3");
+      playAudio("media/audio.mp3", true);
 
       // Change music credit (last slide) if it exists
       var audioCredit = slideshowElems[slideshowElems.length-1].src;
@@ -286,14 +313,18 @@ window.onresize = function() {
 document.addEventListener("DOMContentLoaded", (event) => {
   isMobileDevice = /iPhone|Android|BlackBerry/i.test(navigator.userAgent);
   setPicDimensions();
+  slideIndex = 0;
+
+  // Hide song title if unused
+  if (MANUAL_SLIDESHOW || !SLIDESHOW_AUDIO) {
+    document.getElementById("slideSong").style.display = "none";
+  }
 
   // Initiate slideshow
   if (MANUAL_SLIDESHOW) {
     hidePlayButton();
-    slideIndex = 1;
-    showPic(slideIndex);
+    showPic(1);
   } else {
-    slideIndex = 0;
     slideshow();
   }
 });
